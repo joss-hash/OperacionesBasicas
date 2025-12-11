55a45f8418d7c8bb0a0cf7600b4e12fe5d54b7c9
lopezadair249-arch <lopezadair249@gmail.com>
2025-12-11 10:40:54 -0600

    multiplicacion hecha poe adair

---
+++ multiplicacion.py
@@ -0,0 +1,10 @@
+def multiplicacion(a, b, c):
+  return a * b * c
+
+# Ejemplo de uso
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+resultado = multiplicacion(num1, num2, num3)
+print("Resultado:", resultado) 
