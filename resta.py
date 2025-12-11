0c0776406684114443be6a6142809da3b8485b06
kc2636242-cmd <kc2636242@gmail.com>
2025-12-11 09:07:15 -0600

    Resta py hecho por Karla

---
+++ resta.py
@@ -0,0 +1,9 @@
+def resta():
+    numero1 = float(input("ingresa el primer numero: "))
+
+    numero2 = float(input("ingresa el segundo numero: "))
+
+    numero3 = float(input("ingresa el tercer numero: "))
+
+    resultado = numero1 - numero2 - numero3
+    print("la resta es" , resultado)
